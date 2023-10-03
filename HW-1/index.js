1d1aebb0393ac5d90c9ee57cc0af609a7a43af0b
YuliiaHord <hordiichenko02@gmail.com>
2023-10-03 15:30:26 +0300

    Added HW-1

---
+++ HW-1/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+alert("Hello!");
+let name = prompt("What is your name?");
+let country = prompt("Where are you from?");
+alert(`Nice to meet you, ${name}! You are from ${country}!`);
