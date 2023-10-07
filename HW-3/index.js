c36566161afdb68231900ac038474277f717a19e
YuliiaHord <hordiichenko02@gmail.com>
2023-10-07 22:06:16 +0300

    added HW-3

---
+++ HW-3/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const hour = prompt("Please, enter your hour to calculate:");
+const minutesPerHour = 60;
+const secondsPerMinute = hour * minutesPerHour * 60;
+alert(secondsPerMinute);
