500e5ac236b640643d02aa998134624757dba84d
YuliiaHord <hordiichenko02@gmail.com>
2023-10-15 14:38:46 +0300

    added HW-5

---
+++ HW-5/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+alert("Welcome to calculator!");
+const userChoice = prompt("What action you want to do? \n\nAdd, Diff, Mult, Div, Sqrt, Sin, Cos", "Add");
+
+let operandFirst = 0;
+let operandSecond = 0;
+
+const isTwoOperands = userChoice === "Add" || userChoice === "Diff" || userChoice === "Mult" || userChoice === "Div";
+const isOneOperand = userChoice === "Sin" || userChoice === "Cos" || userChoice === "Sqrt";
+
+if (isTwoOperands) {
+     operandFirst = prompt("Enter first number", "0");
+     operandSecond = prompt("Enter second number", "0");
+} else if (isOneOperand) {
+    operandFirst = prompt("Enter your number");
+} else {
+    alert("Goodbye, see you later");
+}
+
+if ((operandFirst === null || operandSecond === null) || operandFirst === "" || operandSecond === "") {
+    alert("Goodbye, see you later");
+} else if ((!Number(operandFirst) && Number(operandFirst)!==0) || (!Number(operandSecond) && Number(operandSecond)!==0)){
+    alert("This is bad digit, goodbye");
+} else {
+    if (userChoice === "Add") {
+        const sum = Number(operandFirst) + Number(operandSecond);
+        alert(`The sum of ${operandFirst} and ${operandSecond} is ${sum}`);
+    } else if( userChoice === "Diff") {
+        const diff = operandFirst - operandSecond;
+        alert(`The diff of ${operandFirst} and ${operandSecond} is ${diff}`);
+    } else if (userChoice === "Mult") {
+        const mult = operandFirst * operandSecond;
+        alert(`The mult of ${operandFirst} and ${operandSecond} is ${mult}`);
+    } else if (userChoice === "Div") {
+        const div = operandFirst / operandSecond;
+        alert(`The division of ${operandFirst} and ${operandSecond} is ${div}`);
+    } else if (userChoice === "Sqrt") {
+        const sqrt = Math.sqrt(operandFirst);
+        alert(`The square root of ${operandFirst} is ${sqrt}`);
+    } else if (userChoice === "Sin") {
+        const sin = Math.sin(operandFirst);
+        alert(`The sinus of ${operandFirst} is ${sin}`);
+    } else if (userChoice === "Cos") {
+        const cos = Math.cos(operandFirst);
+        alert(`The cosinus of ${operandFirst} is ${cos}`);
+    }
+}
