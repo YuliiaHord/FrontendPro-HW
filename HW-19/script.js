b0e570c1fac0f494fb7c68c8ddf760ddd1672193
YuliiaHord <hordiichenko02@gmail.com>
2023-11-23 13:06:18 +0200

    fixed HW-19

--- HW-19/script.js
+++ HW-19/script.js
@@ -1,39 +1,74 @@
 class SuperMath {
     operations = {
-        "+" : function (x,y) {
-            return x + y;
+        "+": {
+            "add": function (x, y) {
+                return x + y;
+            }
         },
-        "-" : function (x,y) {
-            return x - y;
+        "-": {
+            "subtract": function (x, y) {
+                return x - y;
+            }
         },
-        "*" : function (x,y) {
-            return x * y;
+        "*": {
+            "multiply": function (x, y) {
+                return x * y;
+            }
         },
-        "/" : function (x,y) {
-            return x / y;
+        "/": {
+            "divide": function (x, y) {
+                return x / y;
+            }
         },
-        "%" : function (x,y) {
-            return x % y;
+        "%": {
+            "modulo": function (x, y) {
+                return x % y;
+            }
         }
     }
+
     check(obj) {
-        if(!("x" in obj) || !("y" in obj)){
-            console.log("All numbers are required!");
+        if (!("x" in obj) || !("y" in obj)) {
+            alert("All numbers are required!");
             return;
         }
-        if(!("znak" in obj)) {
-            console.log("There is no sign in your request.")
+        if (!("znak" in obj)) {
+            alert("There is no sign in your request.")
             return;
         }
-        if(!(obj.znak in this.operations)) {
-            console.log("There are no such operations");
+        if (!(obj.znak in this.operations)) {
+            alert("There are no such operations");
             return;
         }
-        return this.operations[obj.znak](obj.x, obj.y);
+        const operation = this.operations[obj.znak];
+        const operationName = Object.keys(operation)[0];
+        if (!(confirm(`Do you want to ${operationName} these digits?`))) {
+            return this.check(this.input());
+        }
+        return operation[operationName](obj.x, obj.y);
+    }
+
+    input(x = null) {
+        if (x === null) {
+            x = parseFloat(prompt("Enter your first digit", 0));
+            if (Number.isNaN(x)) {
+                alert("Please add valid first digit");
+                return this.input();
+            }
+        }
+        const y = parseFloat(prompt("Enter your second digit", 0));
+        if (Number.isNaN(y)) {
+            alert("Please add valid second digit");
+            return this.input(x);
+        }
+        const znak = prompt("Enter your sign (+, -, *, /, %)");
+        return {"x": x, "y": y, "znak": znak};
     }
 }
 
 
 const myMath = new SuperMath();
-console.log(myMath.check({x:2, y:3, "znak": "%"}));
-console.log(myMath.check({x:4, y:2, "znak": "+"}));
+const result = myMath.check(myMath.input());
+if (result !== undefined) {
+    alert(result);
+}
